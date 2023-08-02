b7ce10eeef59a497209181c3ce3381bdbf6efdb9
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-08-01 23:29:06 -0400

    w2

--- SE251.14/w1/pong/js/Player.js
+++ SE251.14/w1/pong/js/Player.js
@@ -1,18 +1,10 @@
 class Player
 {
-    constructor(name, score = 0, highScore = 0, pad = 0)
+    constructor()
     {
-        this.name = name;
-        this.score = score;
-        this.highScore = highScore;
-        this.pad = pad;
+        this.name;
+        this.score = 0;
+        this.highScore = 0;
+        this.pad;
     }
 }
-
-// Example usage:
-const player1 = new Player("name");
-console.log(player1.name);       // Output: Name
-console.log(player1.score);      // Output: 0 (default value)
-console.log(player1.highScore);  // Output: 0 (default value)
-console.log(player1.pad);        // Output: 0 (default value)
-      
--- SE251.14/w1/pong/js/pong.js
+++ SE251.14/w1/pong/js/pong.js
@@ -8,16 +8,27 @@ var timer = setInterval(main, 1000/60)
 //global friction variable
 var fy = .97
 
-//p1 setup
-var p1 = new Box();
-p1.w = 20
-p1.h = 150
-p1.x = 0 + p1.w/2
+//player setup
+var player = [new Player(), new Player()]
+player[0].pad = new Box();
+player[1].pad = new Box();
 
-var p2 = new Box();
-p2.w = 20
-p2.h = 150
-p2.x = 780 + p2.w/2
+var pad = [player[0], pad, player[1], pad]
+
+console.log(player)
+
+//player 1 setup
+pad[0].w = 20
+pad[0].h = 150
+pad[0].x = 0 + pad[0].w/2
+pad[0].pad
+
+//player 2 setup
+pad[1].w = 20
+pad[1].h = 150
+pad[1].x = c.width - pad[1].w/2
+pad[1].color = `blue`
+pad[1].dir = -1
 
 //ball setup
 var ball = new Box();
@@ -27,13 +38,6 @@ ball.vx = -2
 ball.vy = -2
 ball.color = `black`
 
-//score setup
-var score = new Box();
-score.w = 15
-score.h = 15
-score.vx = 0
-score.vy = 0
-score.color = `blue`
 
 function main()
 {
@@ -43,99 +47,83 @@ function main()
     //p1 accelerates when key is pressed 
     if(keys[`w`])
     {
-       p1.vy += -p1.force
+       pad[0].vy += -pad[0].force
     }
 
     if(keys[`s`])
     {
-        p1.vy += p1.force
+        pad[0].vy += pad[0].force
     }
-    //applies friction
-    p1.vy *= fy
-    //player movement
-    p1.move();
-
-    //ball movement
-    ball.move()
 
     //p2 accelerates when key is pressed 
     if(keys[`ArrowUp`])
     {
-       p2.vy += -p2.force
+        pad[1].vy += -pad[1].force
     }
 
     if(keys[`ArrowDown`])
     {
-        p2.vy += p2.force
+        pad[1].vy += pad[1].force
     }
-    //applies friction
-    p2.vy *= fy
-    //player movement
-    p2.move();
 
     //ball movement
     ball.move()
 
-    //p1 collision
-    if(p1.y < 0+p1.h/2)
-    {
-        p1.y = 0+p1.h/2
-    }
-    if(p1.y > c.height-p1.h/2)
-    {
-        p1.y = c.height-p1.h/2
-    }
 
-    //p2 collision
-    if(p2.y < 0+p2.h/2)
-    {
-        p2.y = 0+p2.h/2
-    }
-    if(p2.y > c.height-p2.h/2)
-    {
-        p2.y = c.height-p2.h/2
-    }
-
-    //ball collision 
+    //score
     if(ball.x < 0)
     {
         ball.x = c.width/2
         ball.y = c.height/2
+        player[1].score += 1
     }
+
     if(ball.x > c.width)
     {
-        ball.x = c.width
-        ball.vx = -ball.vx
+        ball.x = c.width/2
+        ball.y = c.height/2
+        player[0].score += 1
     }
+
     if(ball.y < 0)
     {
-        ball.y = 0
+        ball.y = 0 + ball.h/2
         ball.vy = -ball.vy
     }
-    if(ball.y > c.height)
+
+    if(ball.y > c.width)
     {
-        ball.y = c.height
+        ball.y = c.width - ball.h/2
         ball.vy = -ball.vy
-       
     }
 
-    //p1 with ball collision
-    if(ball.collide(p1))
-    {
-        ball.x = p1.x + p1.w/2 + ball.w/2
-        ball.vx = -ball.vx;
-    }
-
-    //p2 with ball collision
-    if(ball.collide(p2))
-    {
-        ball.x = p2.x - p2.w/2 - ball.w/2
-        ball.vx = -ball.vx;
-    }
-
-    //draw the objects
-    p1.draw()
-    p2.draw()
+    console.log(`$(player[0].score) | $(player[1].score)`)
+    var score = document.querySelectorAll(`#score div`)
     ball.draw()
-    score.draw()
+
+    for(i=0; i <score.length; i++)
+    {
+        pad[i].vy *= fy
+        pad[i].move()
+
+        if(pad[i].y < 0+pad[i].h/2)
+        {
+            pad[i].y = 0+pad[i].h/2
+        }
+
+        if(pad[i].y > c.height-pad[i].h/2)
+        {
+            pad[i].y = c.height-pad[i].h/2
+        }
+
+        if (ball.collide(pad[i]))
+        {
+            ball.x = pad[i].x + (pad[i].w/2 + ball.w/2)*pad[i].dir
+            ball.vx = -ball.vx
+        }
+
+        pad[i].draw()
+
+        score[i].innerHTML = player[i].score
+    }
 }
