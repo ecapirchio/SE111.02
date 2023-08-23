d37d8321c22bd3c9601fb13d6eeb7ddcdfcb978b
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-08-23 13:24:30 -0400

    w6

--- SE251.14/w3/pong/js/options.js
+++ SE251.14/w3/pong/js/options.js
@@ -3,8 +3,7 @@
     . on click
     . show or hide the `.sides` div
 ---------*/
-
-var options = document.querySelectorAll("#options h2")
+var options = document.querySelectorAll(`#options h2`)
 options[0].addEventListener(`click`, showhide)
 
 function showhide()
@@ -23,26 +22,26 @@ function showhide()
 
 -----------*/
 var fill = document.querySelectorAll(`.fill`)
-for(let i = 0; i < fill.length; i++)
+for(let i = 0; i<fill.length; i++)
 {
     fill[i].value = player[i][fill[i].classList[0]]
     fill[i].nextElementSibling.innerHTML = player[i][fill[i].classList[0]]
     fill[i].addEventListener(`input`, colorfill)
     function colorfill(e)
     {
+        var word = e.target.classList[0]
         console.dir(e.target.value)
-        player[i].fill = e.target.value
-        player[i].pad.fill = e.target.value
-        e.target.nextElementSibling.innerHTML = player[i].fill
+        player[i][word] = e.target.value
+        player[i].pad[word] = e.target.value
+        e.target.nextElementSibling.innerHTML = player[i][word]
         console.log(fill[i])
     }
 }
-
 var stroke = document.querySelectorAll(`.stroke`)
-for(let i = 0; i < stroke.length; i++)
+for(let i = 0; i<stroke.length; i++)
 {
-    stroke[i].value = player[i][stroke[i].classList[0]]
-    stroke[i].nextElementSibling.innerHTML = player[i][stroke[i].classList[0]]
+    stroke[i].value = player[i].stroke
+    stroke[i].nextElementSibling.innerHTML = player[i].stroke
     stroke[i].addEventListener(`input`, colorfill)
     function colorfill(e)
     {
@@ -54,7 +53,6 @@ for(let i = 0; i < stroke.length; i++)
     }
 }
 
-
 /*---------
     Program the six key inputs to do the following:
     . Display the correct key names for each player   
@@ -63,3 +61,60 @@ for(let i = 0; i < stroke.length; i++)
         .Change the player's key to the value of the input
         .Show the player's key in the output div 
 -----------*/
+var u = document.querySelectorAll(`.u`)
+for(let i = 0; i<u.length; i++)
+{
+    u[i].value = player[i].keys
+    u[i].addEventListener(`keydown`, (e)=>{
+        u[i].value = e.key
+        player[i].keys.u = e.target.value
+        u[i].nextElementSibling.innerHTML = e.target.value
+    })
+    u[i].addEventListener(`focus`, (e)=>{
+        currentState = `pause`
+    })
+}
+
+var d = document.querySelectorAll(`.d`)
+for(let i = 0; i<d.length; i++)
+{
+    d[i].value = player[i].keys
+    d[i].addEventListener(`keydown`, (e)=>{
+        d[i].value = e.key
+        player[i].keys.d = e.target.value
+        d[i].nextElementSibling.innerHTML = e.target.value
+    })
+    d[i].addEventListener(`focus`, (e)=>{
+        currentState = `pause`
+    })
+}
+
+var s = document.querySelectorAll(`.s`)
+for(let i = 0; i<s.length; i++)
+{
+    s[i].value = player[i].keys
+    s[i].addEventListener(`keydown`, (e)=>{
+        s[i].value = e.key
+        player[i].keys.s = e.target.value
+        s[i].nextElementSibling.innerHTML = e.target.value
+    })
+    s[i].addEventListener(`focus`, (e)=>{
+        currentState = `pause`
+    })
+}
+
+var power = document.querySelectorAll(`.power`)
+for(let i = 0; i<power.length; i++)
+{
+    power[i].value = player[i].power
+    power[i].nextElementSibling.innerHTML = player[i].power
+    power[i].addEventListener(`input`, numbuh)
+    function numbuh(e)
+    {
+        console.dir(e.target.value)
+        player[i].power = e.target.value
+        player[i].pad.power = e.target.value
+        e.target.nextElementSibling.innerHTML = player[i].power
+        console.log(power[i])
+    }
+}
