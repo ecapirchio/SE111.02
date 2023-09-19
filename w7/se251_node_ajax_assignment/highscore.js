4e86bd1425a9e0cfa72e994d67e0b7da139a4809
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-09-19 13:04:07 -0400

    w8

--- w7/se251_node_ajax_assignment/highscore.js
+++ w7/se251_node_ajax_assignment/highscore.js
@@ -54,14 +54,59 @@ app.post('/jeep', async (req, res) => {
 });
 
 app.post('/delete', async (req, res) => { 
+    var oldData =  await readFile(`./data/jeep.json`)
+    var newData =  await JSON.parse(oldData)
+    var change = req.params.id.replace(":", ``);
+    console.log(change)
+    newData.splice(change, 1)
+    console.log(newData)
+      const jsonString = JSON.stringify(newData);
+      console.log(jsonString)
+      fs.writeFile('./data/jeep.json', jsonString, err => {
+        if (err) {
+          console.log('Error')
+        }
+        else{
+          console.log('Success')
+        }
+      });
+
+      res.send(jsonString);
   //add the delete functionality here.
   //read in the jeep.json file
   //splice out the correct index from the array
   //write the file again
+  /*try {
+    // Read the jeep.json file
+    const rawData = fs.readFileSync('jeep.json');
+    const jeeps = JSON.parse(rawData);
+
+    // Assuming you have a parameter in the request body that specifies which item to delete (e.g., req.body.id)
+    const itemIdToDelete = req.body.id;
+
+    // Find the index of the item with the specified ID
+    const indexToDelete = jeeps.findIndex((jeep) => jeep.id === itemIdToDelete);
+
+    // Check if the item exists
+    if (indexToDelete === -1) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+
+    // Remove the item from the array using splice
+    jeeps.splice(indexToDelete, 1);
+
+    // Write the updated data back to the jeep.json file
+    fs.writeFile('jeep.json', JSON.stringify(jeeps, null, 2));
+
+    return res.status(200).json({ message: 'Item deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }*/
 });
 
 //Start up the server on port 3000.
-var port = process.env.PORT || 80
+var port = process.env.PORT || 3000
 app.listen(port, ()=>{
-    console.log("Server Running at Localhost:80")
+    console.log("Server Running at Localhost:3000")
 })
